bd75f04cd2d71152d752a6c6463ec6ac5fae235b
Gulen Chongtham <gulench@gmail.com>
2016-12-26 01:34:05 +0530

    Refactor acceptance tests to use helper functions

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -42,13 +42,13 @@ test('List bands', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/bands');
 
-    assert.equal(find('.band-link').length, 2, 
+    assertLength(assert, '.band-link', 2, 
       'All band links are rendered');
 
-    assert.equal(find('.band-link:contains("Radiohead")').length, 1, 
+    assertLength(assert, '.band-link:contains("Radiohead")', 1, 
       'First band link contains the band name');
 
-    assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 
+    assertLength(assert, '.band-link:contains("Long Distance Calling")', 1, 
       'The other band link contains the band name');
   });
 });
@@ -99,9 +99,9 @@ test('Create a new band', function(assert) {
   click('.new-band-button');
 
   andThen(function() {
-    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
-    assert.equal(find('.band-link:last').text().trim(), 'Long Distance Calling', 'Created band appears at the end of the list');
-    assert.equal(find('.nav a.active:contains("Songs")').length, 1, 'The Songs tab is active');
+    assertLength(assert, '.band-link', 2, 'All band links are rendered');
+    assertTrimmedText(assert, '.band-link:last', 'Long Distance Calling', 'Created band appears at the end of the list');
+    assertLength(assert, '.nav a.active:contains("Songs")', 1, 'The Songs tab is active');
   });
 });
 
@@ -164,7 +164,6 @@ test('Create a new song in two steps', function (assert) {
   triggerEvent('.new-song-form', 'submit');
 
   andThen(function () {
-    assert.equal(find('.songs .song:contains("Killer Cars")').length,
-      1, "Creates the song and displays it in the list");
+    assertElement(assert, '.songs .song:contains("Killer Cars")', "Creates the song and displays it in the list");
   });
 });
